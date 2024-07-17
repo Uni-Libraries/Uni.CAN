f091180b8c39ee9c9652dbf98690b3e301ddc893
Uni Libraries <team@uni-libraries.net>
2024-07-17 14:01:30 +0300

    cmake: fix uni.common repo name

--- cmake/cpm-uni.common.cmake
+++ cmake/cpm-uni.common.cmake
@@ -1,6 +1,6 @@
 CPMAddPackage(
         NAME uni.common
-        GITHUB_REPOSITORY catchorg/Catch2
+        GITHUB_REPOSITORY Uni-Libraries/Uni.Common
         GIT_TAG ca2fcf2764f234c6f62c8da28a8d956d1fca6c17
         OPTIONS
             "BUILD_SHARED_LIBS OFF"
