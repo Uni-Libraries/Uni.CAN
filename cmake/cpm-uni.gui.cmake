f29ecad847bad02d2367067104bca57f3275e2f9
Mikhail Paulyshka <me@mixaill.net>
2025-10-21 20:12:08 +0300

    cmake: remove uni.gui

--- cmake/cpm-uni.gui.cmake
+++
@@ -1,8 +0,0 @@
-CPMAddPackage(
-        NAME uni.gui
-        GITHUB_REPOSITORY uni-libraries/uni.gui
-        GIT_TAG 973cd9a1dd9f735296139be77c7e22bf951a6c84
-        VERSION 2025.05.19
-        OPTIONS
-            "BUILD_SHARED_LIBS OFF"
-)
