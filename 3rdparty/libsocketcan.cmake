756f484a2a8d8d754e7680a37102ed795f998d82
Mikhail Paulyshka <me@mixaill.net>
2026-01-20 04:58:49 -0800

    3rdparty: fix windows build

--- 3rdparty/libsocketcan.cmake
+++ 3rdparty/libsocketcan.cmake
@@ -1,4 +1,5 @@
-
-add_library(libsocketcan STATIC)
-target_include_directories(libsocketcan PUBLIC "${CMAKE_CURRENT_LIST_DIR}/libsocketcan/include")
-target_sources(libsocketcan PRIVATE "${CMAKE_CURRENT_LIST_DIR}/libsocketcan/src/libsocketcan.c")
+if(NOT WIN32)
+    add_library(libsocketcan STATIC)
+    target_include_directories(libsocketcan PUBLIC "${CMAKE_CURRENT_LIST_DIR}/libsocketcan/include")
+    target_sources(libsocketcan PRIVATE "${CMAKE_CURRENT_LIST_DIR}/libsocketcan/src/libsocketcan.c")
+endif()
